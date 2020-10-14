42a4deb0327608669ca7cdb9f44a07e998b1e744
Brian <brianvanbeusekom@hotmail.com>
2020-10-14 20:59:37 +0200

    startmenu test 1

---
+++ VR Experiment/Assets/Scripts/startmenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startmenu : MonoBehaviour
+{
+    public GameObject MenuElement;
+    public GameObject tutorialElement;
+
+    public GameObject StartButton;
+    public GameObject QuitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (MenuElement)
+            MenuElement.SetActive(true);
+        if (tutorialElement)
+            tutorialElement.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject == StartButton && OVRInput.Get(OVRInput.Button.Two))
+        {
+            //start the game
+            MenuElement.SetActive(false);
+            tutorialElement.SetActive(true);
+        }
+
+        if(other.gameObject == QuitButton) { }
+    }
+
+}
